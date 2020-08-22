55159279e00ffcdf60dc5eb29c7adddf6234dde1
pullso <p.dubitskiy@ya.ru>
2020-08-22 14:36:36 +0500

    add vuelidate for Login.vue

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate/src';
 import dateFilter from '@/filters/date.filter';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
 Vue.filter('date', dateFilter);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
